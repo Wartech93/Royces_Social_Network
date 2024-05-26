6398d6d75bcb77ff036506570dcc30096b31794f
Sweaty Hog <Royceatkins93@gmail.com>
2024-05-26 14:32:19 -0400

    create a thoughtschema for reactions count

--- models/Thought.js
+++ models/Thought.js
@@ -24,10 +24,14 @@ const thoughtSchema = new Schema(
   {
     toJSON: {
       getters: true,
+      virtuals: true
     },
     versionKey: false
   }
 );
+thoughtSchema.virtual('reactionCount').get(function() {
+  return this.reactions.length;
+});
 
 const Thought = model('thought', thoughtSchema);
 
